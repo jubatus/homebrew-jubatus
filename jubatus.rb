7ec2f1ddc804ac4e7869330c1092e021c7bb70e5
Shohei Hido <hido@preferred.jp>
2013-11-22 22:18:00 +0900

    Update formula for 0.5.0

--- jubatus.rb
+++ jubatus.rb
@@ -31,28 +31,35 @@ class ZooKeeperRequirement < Requirement
 end
 
 class Jubatus < Formula
-  url 'https://github.com/jubatus/jubatus/tarball/0.4.5'
-  head 'https://github.com/jubatus/jubatus.git'
+  # url 'https://github.com/jubatus/jubatus/tarball/0.5.0'
+  head 'https://github.com/jubatus/jubatus.git', :revision => '21ce129c1259a8070262bfdaf0a3002ea0b6eba2'
   homepage 'http://jubat.us/'
-  sha1 '104daca02a1f31f0e29ebbd8f45c7ed4200baaad'
-  version '0.4.5'
+  # sha1 'ae06f9b0a6dc39c6b37f9de1bb74ea874d231a25'
+  version '0.5.0'
 
-  option 'disable-re2', 'Stop using re2 for regx'
-  option 'enable-mecab', 'Using mecab for Japanese NLP'
-  option 'enable-zookeeper', 'Using zookeeper for distributed environemnt'
+  option 'disable-onig', 'Disable oniguruma for regex'
+  option 'enable-mecab', 'Enable mecab for Japanese NLP'
+  option 'enable-zookeeper', 'Enable ZooDeeper for distributed environemnt'
 
   depends_on 'glog'
   depends_on 'pkg-config'
-  depends_on 'pficommon'
   depends_on 'jubatus-msgpack-rpc'
 
   depends_on ZooKeeperRequirement.new if build.include? 'enable-zookeeper'
   depends_on 'mecab' if build.include? "enable-mecab"
-  depends_on 're2' unless build.include? "disable-re2"
+  depends_on 'oniguruma' unless build.include? "disable-onig"
   # snow leopard default gcc version is 4.2
   depends_on 'gcc' if build.include? 'snow-leopard'
 
   def install
+    # does not support Mavenricks                                                                                                                                
+    if MacOS.version >= "10.9"
+      <<-EOS.undent
+        Jubatus currently does not support Mavericks or later version
+      EOS
+      system "Jubatus currently does not support Mavericks or later version"
+      onoe "Error! Jubatus currently does not support Mavericks or later version"
+    end
     if ENV.compiler == :gcc
       gcc = Formula.factory('gcc')
       version = '4.7'
@@ -68,7 +75,7 @@ class Jubatus < Formula
     STDERR.puts ENV['CC'], ENV['CXX']
     args = []
     args << "--prefix=#{prefix}"
-    args << "--disable-re2" if build.include? "disable-re2"
+    args << "--disable-onig" if build.include? "disable-onig"
     args << "--enable-mecab" if build.include? "enable-mecab"
     args << "--enable-zookeeper" if build.include? "enable-zookeeper"
     system "./waf", "configure", *args
