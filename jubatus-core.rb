9049a9eddb6ae3797d85c07594f7c2a4d5263aa9
Gentaro Watanabe <g.wtnb@preferred.jp>
2014-06-24 19:01:09 +0900

    Patch for Mountain Lion

--- jubatus-core.rb
+++ jubatus-core.rb
@@ -19,6 +19,11 @@ class JubatusCore < Formula
   # snow leopard default gcc version is 4.2
   depends_on 'gcc' if build.include? 'snow-leopard'
 
+  patch do
+    url 'https://gist.githubusercontent.com/gwtnb/beb8abd33f99e6452e4c/raw/1144be89646a193a2061ad28015212d0a9c48b14/isinf.diff'
+    sha1 'd8755b2ff2437b61c23bce27c137236103f84c06'
+  end
+
   def install
     if MacOS.version >= "10.9"
       ENV['CXXFLAGS'] = '-std=c++11'
